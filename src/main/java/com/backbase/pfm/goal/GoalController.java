2da29caf313874d23a3e10d6db123a970c28d2ea
Zoltan Altfatter <altfatterz@gmail.com>
2014-04-09 00:58:02 +0200

    add POST and PUT

--- src/main/java/com/backbase/pfm/goal/GoalController.java
+++ src/main/java/com/backbase/pfm/goal/GoalController.java
@@ -1,10 +1,9 @@
 package com.backbase.pfm.goal;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @RestController
@@ -18,14 +17,37 @@ public class GoalController {
         this.goalRepository = goalRepository;
     }
 
-    @RequestMapping
+    @RequestMapping(method = RequestMethod.GET)
     public List<Goal> getGoals() {
         return goalRepository.findAll();
     }
 
-    @RequestMapping("/{id}")
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public Goal getGoal(@PathVariable Long id) {
         return goalRepository.findOne(id);
     }
+
+
+    // curl -H "Content-Type:application/json" -X POST localhost:8080/v1/pfm/customers -d '{"name":"New Car","amount":1000}'
+    @RequestMapping(method = RequestMethod.POST)
+    public void createGoal(@RequestBody Goal goal) {
+        goalRepository.save(goal);
+    }
+
+    // TODO: does not work yet
+    // curl -H "Content-Type:application/json" -X PUT localhost:8080/v1/pfm/customers/4 -d '{"name":"Vacation","amount":500}'
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public void updateGoal(@RequestBody Goal goal) {
+        System.out.println(goal.getName() + " : " + goal.getAmount());
+        goalRepository.save(goal);
+    }
+
+
+
+
+
+
+
+
 
 }
